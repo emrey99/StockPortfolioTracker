0a5237fe760aa19a1360b2f3b7047c7b1bf38f63
emrey99 <yashiemre99@gmail.com>
2021-11-21 08:23:53 +0200

    Trader validations created

--- src/common/constants/ExceptionMessages.java
+++ src/common/constants/ExceptionMessages.java
@@ -1,4 +1,10 @@
 package common.constants;
 
 public class ExceptionMessages {
+
+    public static final String INVALID_BUDGET = "Budget can not be less or equal than 0.";
+    public static final String INVALID_TRADER_ID = "Trader id can not be less or equal thank 0.";
+    public static final String INVALID_TRADER_NAME = "Trader can not be empty.";
+    
+
 }
--- src/models/traders/BaseTrader.java
+++ src/models/traders/BaseTrader.java
@@ -1,5 +1,7 @@
 package models.traders;
 
+import static common.constants.ExceptionMessages.*;
+
 public class BaseTrader implements Trader {
 
     private int id;
@@ -19,6 +21,10 @@ public class BaseTrader implements Trader {
     }
 
     public void setId(int id) {
+        if (id <= 0){
+            throw new IllegalArgumentException(INVALID_TRADER_ID);
+        }
+
         this.id = id;
     }
 
@@ -28,6 +34,10 @@ public class BaseTrader implements Trader {
     }
 
     public void setName(String name) {
+        if (name == null || name.trim().isEmpty()){
+            throw new IllegalArgumentException(INVALID_TRADER_NAME);
+        }
+
         this.name = name;
     }
 
@@ -37,6 +47,10 @@ public class BaseTrader implements Trader {
     }
 
     public void setBudget(int budget) {
+        if (budget <= 0){
+            throw new IllegalArgumentException(INVALID_BUDGET);
+        }
+
         this.budget = budget;
     }
 }
