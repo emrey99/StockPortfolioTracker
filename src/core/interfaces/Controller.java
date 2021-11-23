31b2cb772cbb0dd77a1b898a883b7d052daa41b2
emrey99 <yashiemre99@gmail.com>
2021-11-23 14:58:48 +0200

    addStock method added in Controller

--- src/core/interfaces/Controller.java
+++ src/core/interfaces/Controller.java
@@ -4,6 +4,7 @@ public interface Controller {
 
     String addTrader(int id,String name,int budget);
     String removeTrader(int id);
+    String buyStock(String traderName,String stockName,double stockPrice,int stockQuantity);
 
 
 }
